0db2b96f5dec676aab42b394374471b1720c9589
Alii-Isk <65794374+Alii-isk@users.noreply.github.com>
2021-01-01 15:43:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m user1
-sudo adduser user1 sudo
-echo 'user1:userroot' | sudo chpasswd
+sudo useradd -m rdp
+sudo adduser rdp sudo
+echo 'rdp:rdpuser' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
+#download and install chrome-remote-desktop
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
@@ -18,14 +19,16 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser user1 chrome-remote-desktop
+sudo adduser rdp chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - user1 -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n\n'
+su - rdp -c """$CRP"""
+printf 'Access Your Machine here ===> https://remotedesktop.google.com/access/ \n\n'
+printf 'User: rdp'
+printf 'Password: rdpuser'
 if sudo apt-get upgrade &> /dev/null
 then
     printf "\n\nUpgrade Completed " >&2
